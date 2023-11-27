aa0c66fe89a42861af4df12a01d0382ba3bdd92e
shjc4623 <shjc4623@kakao.com>
2023-11-27 06:51:47 +0000

    chore

--- kakao-app/src/App.tsx
+++ kakao-app/src/App.tsx
@@ -1,9 +1,11 @@
 import KakaoMap from "./components/KakaoMap";
 
 function App() {
-	console.log(process.env.REACT_APP_KAKAO_KEY)
+  console.log(process.env.REACT_APP_KAKAO_KEY);
   return (
     <div className="App">
+      <div>KAKAO KEY : {process.env.REACT_APP_KAKAO_KEY}</div>
+      <div>PUBLIC URL : {process.env.PUBLIC_URL}</div>
       <KakaoMap />
     </div>
   );
