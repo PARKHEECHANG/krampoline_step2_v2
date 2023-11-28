77b6cf5a3deac4ab32c5501a7b596da5ac62e676
PARKHEECHANG <shjc4623@gmail.com>
2023-11-28 19:46:44 +0900

    feat:get watch position

--- kakao-app/src/components/KakaoMap.tsx
+++ kakao-app/src/components/KakaoMap.tsx
@@ -1,39 +1,61 @@
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 import { useState, useEffect } from "react";
 
+interface Position {
+  lat: number;
+  lng: number;
+  alt: number | null;
+}
+
 const KakaoMap = () => {
   const [level, setLevel] = useState(3); // 확대 레벨
 
-  const [currentPostion, setCurrentPostion] = useState({
-    center: {
-      lat: 33.55635,
-      lng: 127.0648,
-    },
+  const [currentPosition, setCurrentPosition] = useState<Position>({
+    lat: 0,
+    lng: 0,
+    alt: 0,
   });
 
   useEffect(() => {
+    let watchId: any;
+
     if (navigator.geolocation) {
-      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-      navigator.geolocation.getCurrentPosition((position) => {
-        setCurrentPostion((prev) => ({
-          ...prev,
-          center: {
-            lat: position.coords.latitude, // 위도
-            lng: position.coords.longitude, // 경도
-          },
-        }));
-      });
+      // 사용자의 위치를 지속적으로 추적합니다
+      watchId = navigator.geolocation.watchPosition(
+        (position) => {
+          setCurrentPosition({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+            alt: position.coords.altitude,
+          });
+        },
+        (error) => {
+          console.log(error);
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 3000,
+          maximumAge: 0,
+        }
+      );
     }
   }, []);
 
+  console.log(currentPosition);
+
   return (
-    <Map
-      center={currentPostion.center}
-      style={{ width: "500px", height: "500px" }}
-      level={level} // 지도 확대 레벨
-    >
-      <MapMarker position={currentPostion.center}></MapMarker>
-    </Map>
+    <div>
+      <Map
+        center={{ lat: currentPosition.lat, lng: currentPosition.lng }}
+        style={{ width: "500px", height: "500px" }}
+        level={level} // 지도 확대 레벨
+      >
+        <MapMarker
+          position={{ lat: currentPosition.lat, lng: currentPosition.lng }}
+        ></MapMarker>
+      </Map>
+      <div>현재 고도 : {currentPosition.alt}</div>
+    </div>
   );
 };
 
